a36acbaf022cd32b3a5d83b2ee8aa78c1b413fdb
Markus Eliasson <markus.eliasson@factor10.com>
2018-03-30 09:35:26 +0200

    Setup a DocumentManager to prepare for compiling

--- src/extension.ts
+++ src/extension.ts
@@ -1,29 +1,146 @@
+//
+// Sequence VS Code extension- A simple sequence diagram tool
+//
+// Copyright (C) - markus.eliasson@gmail.com
+//
 'use strict';
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
+
 import * as vscode from 'vscode';
+import * as sequence from '@eliasson/sequence';
+export const vscodeUnderTest = vscode;
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
+// This is the VSC internal scheme user for the generated Sequence diagrams
+const SCHEME = 'sequence';
+const LANGUAGE_ID = 'sequence';
+
 export function activate(context: vscode.ExtensionContext) {
+    // Wrapper around how documents and SVG providers are linked
+    const documentManager = new DocumentManager(); 
 
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "sequence-vscode-ext" is now active!');
+    context.subscriptions.push(
+        vscode.workspace.onDidOpenTextDocument((event: vscode.TextDocument) => {
+            if(event.languageId === LANGUAGE_ID) {
+                documentManager.add({ uri: event.uri, source: event.getText()});
+            }
+        })
+    );
 
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with  registerCommand
-    // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.sayHello', () => {
-        // The code you place here will be executed every time your command is executed
+    context.subscriptions.push(
+        vscode.workspace.onDidCloseTextDocument((event: vscode.TextDocument) => {
+            if(event.languageId === LANGUAGE_ID) {
+                documentManager.remove(event.uri);
+            };
+        })
+    );
 
-        // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World!');
-    });
+    context.subscriptions.push(
+        vscode.workspace.onDidChangeTextDocument((event: vscode.TextDocumentChangeEvent) => {
+            if(event.document.languageId === LANGUAGE_ID) {
+                documentManager.update(event.document.uri, event.document.getText());
+            }
+        })
+    );
 
-    context.subscriptions.push(disposable);
+    context.subscriptions.push(
+        vscode.commands.registerCommand('sequence.previewSvg', () => {
+            const editor = vscode.window.activeTextEditor;
+            if (!editor) return;
+
+            const doc = editor.document;
+            if (doc.languageId !== LANGUAGE_ID) {
+                vscode.window.showInformationMessage('This file is not identified as a Sequence file (.seq).');
+                return;
+            }
+            vscode.window.showInformationMessage('Not yet implemented');
+        })
+    );
 }
 
-// this method is called when your extension is deactivated
 export function deactivate() {
+    // None of the examples contains cleanup code. NONE!
+}
+
+interface SequenceDocument {
+    uri: vscode.Uri;
+    source: string;
+    svgContent?: string;
+    pendingCompilation?: NodeJS.Timer;
+    rejectCompilation?: Function
+}
+
+export class DocumentManager {
+    documents = new Map<string, SequenceDocument>();
+
+    constructor(private debounceTimeoutMs=400) {}
+
+    add(doc: SequenceDocument): Promise<void> {
+        return this.scheduleCompilation(doc);
+    }
+
+    remove(uri: vscode.Uri): Promise<void> {
+        let promise = undefined;
+
+        if(this.documents.has(uri.toString())) {
+            const doc = this.documents.get(uri.toString());
+            this.cancelCompilation(doc);
+            promise = this.scheduleCompilation(doc);
+        }
+
+        this.documents.delete(uri.toString());
+        return promise || Promise.resolve();
+    }
+
+    update(uri: vscode.Uri, source: string) {
+        let promise;
+
+        if(this.documents.has(uri.toString())) {
+            const doc = this.documents.get(uri.toString());
+            doc.source = source;
+
+            this.cancelCompilation(doc);
+            promise = this.scheduleCompilation(doc);
+        }
+        return promise || Promise.reject(`No document with uri ${uri} exists!`);
+    }
+
+    private compileDocument(uri: vscode.Uri) {
+        if(this.documents.has(uri.toString())) {
+            const doc = this.documents.get(uri.toString());
+
+            // Only clear the handles, we do not want to reject the given promise!
+            if(doc.pendingCompilation) doc.pendingCompilation = undefined;
+            if(doc.rejectCompilation) doc.rejectCompilation = undefined;
+        }
+        // TODO: Compile the source code
+        // TODO: Report diagnostic messages to editor
+        // TODO: Store the compiled output and used to preview and generate SVG
+    }
+
+    private scheduleCompilation(doc: SequenceDocument): Promise<void>  {
+        return new Promise((resolve, reject) => {
+            const timer = setTimeout(() => {
+                this.compileDocument(doc.uri);
+                resolve();
+            }, this.debounceTimeoutMs);
+
+            this.documents.set(doc.uri.toString(), {
+                uri: doc.uri,
+                source: doc.source,
+                svgContent: doc.svgContent,
+                pendingCompilation: timer,
+                rejectCompilation: reject
+            });
+        });
+    }
+
+    private cancelCompilation(doc: SequenceDocument) {
+        if(doc.pendingCompilation) {
+            clearTimeout(doc.pendingCompilation);
+            doc.pendingCompilation = undefined;
+        }
+        if(doc.rejectCompilation) {
+            doc.rejectCompilation();
+            doc.rejectCompilation = undefined;
+        }
+    }
 }
--- test/extension.test.ts
+++ test/extension.test.ts
@@ -1,22 +1,181 @@
 //
-// Note: This example test is leveraging the Mocha test framework.
-// Please refer to their documentation on https://mochajs.org/ for help.
+// Sequence VS Code extension- A simple sequence diagram tool
 //
-
-// The module 'assert' provides assertion methods from node
-import * as assert from 'assert';
-
-// You can import and use all API from the 'vscode' module
-// as well as import your extension to test it
+// Copyright (C) - markus.eliasson@gmail.com
+//
+import 'mocha';
+import * as chai from 'chai';
+import * as spies from 'chai-spies';
 import * as vscode from 'vscode';
-import * as myExtension from '../src/extension';
+import * as extension from '../src/extension';
 
-// Defines a Mocha test suite to group tests of similar kind together
-suite("Extension Tests", () => {
+const expect = chai.expect;
+chai.use(spies);
 
-    // Defines a Mocha unit test
-    test("Something 1", () => {
-        assert.equal(-1, [1, 2, 3].indexOf(5));
-        assert.equal(-1, [1, 2, 3].indexOf(0));
+class StubExtensionContext implements vscode.ExtensionContext {
+    subscriptions: { dispose(): any; }[] = new Array();
+    workspaceState: vscode.Memento;
+    globalState: vscode.Memento;
+    extensionPath: string;
+    asAbsolutePath(relativePath: string): string {
+        throw new Error("Method not implemented.");
+    }
+    storagePath: string;
+}
+
+const DEBOUCE_TIMEOUT_MS = 1;
+const SOURCE = 'Name Unit-test';
+const UPDATED_SOURCE = 'Name Unit-test\nActor Alice';
+
+const newDM = () => new extension.DocumentManager(DEBOUCE_TIMEOUT_MS);
+const newUri = (name) => vscode.Uri.parse(`sequence://example/${name}`);
+const newDoc = (name) => { return { uri: newUri(name), source: SOURCE } };
+
+describe('Extension', () => {
+    let context,
+        registerCommandSpy,
+        registerProviderSpy;
+
+    beforeEach(() => {
+        context = new StubExtensionContext();
+        registerCommandSpy = chai.spy(extension.vscodeUnderTest.commands.registerCommand);
+        registerProviderSpy = chai.spy(extension.vscodeUnderTest.workspace.registerTextDocumentContentProvider)
+        extension.activate(context);
+    });
+
+    afterEach(() => {
+        // VSCode does some (unknown) cleanup at deactivation. Try to mimic this
+        // else commads fails to register on second test.
+        context.subscriptions.forEach(it => it.dispose());
+    });
+
+    it('should be loaded', () => {
+        expect(vscode.extensions.getExtension('markuseliasson.sequence-vscode-ext')).to.not.be.undefined;
+    });
+
+    xit('should register compile command', () => {
+        expect(registerCommandSpy).to.have.been.called.with('sequence.previewSvg');
+    });
+});
+
+describe('DocumentManager', () => {
+    describe('at construction', () => {
+        let dm;
+
+        beforeEach(() => {
+            dm = newDM();
+        });
+
+        it('should not contain any documents at first', () => {
+            expect(dm.documents.size).to.equal(0);
+        });
+    });
+
+    describe('when adding a document', () => {
+        let compileDocument,
+            doc,
+            dm;
+
+        beforeEach(() => {
+            doc = newDoc('one.seq');
+            dm = newDM();
+            compileDocument = chai.spy.on(dm, 'compileDocument');
+        });
+
+        it('one document should be added', (done) => {
+            dm.add(doc)
+                .then(() => {
+                    expect(dm.documents.size).to.equal(1);
+                    done();
+                });
+            
+        });
+
+        it('should eventually call `compileDocument`', (done) => {
+            dm.add(doc)
+                .then(() => {
+                    expect(compileDocument).to.have.been.called.with(doc.uri);
+                    done();
+                });
+        });
+    });
+
+    describe('removing a document', () => {
+        let dm;
+
+        beforeEach(() => {
+            dm = newDM();
+        });
+
+        it('one document should be removed', (done) => {
+            dm.add(newDoc('one.seq'))
+                .then(() => dm.add(newDoc('two.seq')))
+                .then(() => dm.remove(newUri('one.seq')))
+                .then(() => {
+                    expect(dm.documents.size).to.equal(1);
+                    done();
+                });
+        });
+
+        it('should cancel ongoing compilation', (done) => {
+            dm.add(newDoc('one.seq'))
+                .then(() => { /* success */ }, done);
+            dm.remove(newUri('one.seq'));
+        });
+    });
+
+    describe('updating a document', () => {
+        let compileDocument,
+            doc,
+            dm;
+
+        beforeEach(() => {
+            doc = newDoc('one.seq');
+            dm = newDM();
+            compileDocument = chai.spy.on(dm, 'compileDocument');
+        });
+
+        it('should eventually call `compileDocument`', (done) => {
+            dm.add(doc)
+                .then(() => {
+                    return dm.update(doc.uri, UPDATED_SOURCE);
+                })
+                .then(() => {
+                    expect(compileDocument).to.have.been.called.with(doc.uri);
+                    done();
+                });
+        });
+
+        it('should cancel a already scheduled compilation', () => {
+            return new Promise((resolve, reject) => {
+                const promiseToAdd = dm.add(doc);
+                const promiseToUpdate = dm.update(doc.uri, UPDATED_SOURCE);
+                promiseToUpdate
+                    .then(() => {
+                        promiseToAdd.then(reject, resolve)
+                    });
+            });
+        });
+
+        it('should update the document immediately', (done) => {
+            dm.add(doc)
+                .then(() => {
+                    // Check the document status immediately, but don't complete
+                    // the test-case until the promise is fulfilled
+                    const promiseToUpdate = dm.update(doc.uri, UPDATED_SOURCE);
+                    const updatedDoc = dm.documents.values().next().value;
+                    expect(updatedDoc.source).to.equal(UPDATED_SOURCE);
+
+                    return promiseToUpdate;
+                })
+                .then(done);
+        });
+
+        it('should fail to update a non-existing document', () => {
+            return new Promise((resolve, reject) => {
+                dm.update(newUri('missing'), UPDATED_SOURCE)
+                    .then(reject, resolve); // Switched since failure is expected
+            });
+        });
     });
 });
